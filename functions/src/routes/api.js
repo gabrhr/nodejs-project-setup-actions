2e174dc63928bd882e23ea4d73fb47172ddf7ff4
Gabriela HR <grhernandez@pucp.edu.pe>
2025-04-20 01:44:25 -0500

    feat(*): update firebase functions folder

--- functions/src/routes/api.js
+++ functions/src/routes/api.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello from the API route!" });
+  res.send("Hello world from the API route! - Gabriela Hernandez");
+});
+
+router.get("/message", (req, res) => {
+  res.json({ message: "This is a message - Gabriela Hernandez" });
 });
 
 module.exports = router;
